fd73008e6ab8277d5667ee2e212241b0f5aadf39
Sean Borland <seanbor123@gmail.com>
2025-02-01 14:28:13 -0500

    popup changes

--- src/components/Popup/Popup.jsx
+++ src/components/Popup/Popup.jsx
@@ -7,15 +7,19 @@ const Popup = ({ characterName, characterId, onClose }) => {
   return (
     <div className="popup-overlay">
       <div className="popup">
+        {/* Close button */}
+        <button className="popup-close-btn" onClick={onClose}>
+          X
+        </button>
         <p className="popup-title">How would you like to explore the character of {characterName}?</p>
         <div className="popup-buttons">
           {/* Learn More */}
           <Link to={`/comics/${characterId}`} state={{ characterName }}>
-            <Button text={`Learn more about ${characterName}`} />
+            <Button text='Learn More About This Character' />
           </Link>
           {/* Explore Comics */}
           <Link to={`/comics/${characterId}`} state={{ characterName }}>
-            <Button text={`Explore comics by ${characterName}`} />
+            <Button text='Explore Comics By This Character' />
           </Link>
           <Button text="Close" onClick={onClose} />
         </div>
